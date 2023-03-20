e713b48b7963ebc4ab99b4c4a76dd7630db37854
Veerachavali <128286435+Veerachavali@users.noreply.github.com>
2023-03-20 11:51:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('my parents new helthy diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('omega3 & Blueberry otameal')
+streamlit.text('kale,spinch,rocket smoothy')
