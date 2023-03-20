112366ee8246c0bca9c57419317a02eec60705dc
Veerachavali <128286435+Veerachavali@users.noreply.github.com>
2023-03-20 12:36:22 +0530

    Update streamlit_app.py
    Added breakfast menu and hearder and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
+
 streamlit.title('my parents new helthy diner')
+
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('🐔omega3 & Blueberry otameal')
 streamlit.text('🥗kale,spinch,rocket smoothy')
 streamlit.text('🥑🍞Hard boiled-free range egg')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇') 
 
 import pandas
